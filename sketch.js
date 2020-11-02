2edc53f0ef64b1ab446259ca3b445fcc63a370fb
kra-ti <72553178+kra-ti@users.noreply.github.com>
2020-11-02 17:27:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var wall;
+var bullet,speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  
+  speed=random(30,50)
+  weight=random(30,52)
+
+thickness=random(22,83)
+
+bullet=createSprite(50,200,50,5)
+bullet.velocityX=speed
+bullet.shapeColor=color(255)
+
+wall=createSprite(1200,200,thickness,height/2)
+wall.shapeColor=color(230,230,230)
+
+}
+
+function draw() {
+  background(0);  
+
+  if (hasCollided(bullet,wall)) {
+    bullet.velocityX=0
+  var damage=-2*weight*speed*speed/(thickness*thickness*thickness)
+  
+  if (damage>10) {
+   
+    wall.shapeColor=color(100,100,100)
+  }
+  if (damage<10) {
+    wall.shapeColor=color(200,200,200)
+  }
+  }
+   
+
+
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x+lbullet.width
+  wallLeftEdge=lwall.x
+  if (bulletRightEdge>=wallLeftEdge) {
+    return true
+  }
+  return false
+}
+  
+
+   
+  
